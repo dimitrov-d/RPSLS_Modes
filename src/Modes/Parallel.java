5acaf2d53d9035296c45b0259965a8a7ca3026d8
Damjan <damjandimitrov1@gmail.com>
2020-02-09 19:50:08 +0100

    Threads have references to players array and numGames

--- src/Modes/Parallel.java
+++ src/Modes/Parallel.java
@@ -38,18 +38,25 @@ class TPlayer extends Thread
 	@Override
 	public void run()
 	{
+		TPlayer players[] = Parallel.players;
+		int numGames = Parallel.numGames;
+		
 		String name = Thread.currentThread().getName();
 		int playerNum = Character.getNumericValue(name.charAt(name.length() - 1));
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < players.length; i++)
 		{
 			if (i == playerNum)
 				continue;
-			playGame(Parallel.players[playerNum], Parallel.players[i]);
+			for (int j = 0; j < numGames; j++)
+			{
+				playGame(Parallel.players[playerNum], Parallel.players[i]);
+			}
 		}
 	}
 
 	public void playGame(TPlayer p1, TPlayer p2)
 	{
 		System.out.println(p1.getName() + " vs " + p2.getName());
+		
 	}
 }
